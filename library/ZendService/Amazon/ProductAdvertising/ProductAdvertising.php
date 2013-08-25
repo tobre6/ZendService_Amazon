4c11f6d92fb3b77f0ad33c760bad3a4033028429
tobre <tobre@dorpat>
2013-08-26 00:33:45 +0300

    Test
    Change-Id: I8238860e8f0463050d905673c377a1b72ce9ba3a

--- library/ZendService/Amazon/ProductAdvertising/ProductAdvertising.php
+++ library/ZendService/Amazon/ProductAdvertising/ProductAdvertising.php
@@ -93,6 +93,7 @@ class ProductAdvertising
      */
     public function __construct($accessKeyId, $secretKey = null, $associateTag = null, $countryCode = 'US', $version = null)
     {
+        echo 'aa';
         $this->accessKeyId = (string) $accessKeyId;
         $this->secretKey = $secretKey;
         $this->associateTag = $associateTag;
