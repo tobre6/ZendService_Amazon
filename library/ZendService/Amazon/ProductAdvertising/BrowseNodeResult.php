459b933d2820d65bc0be824274508ec6fbb9e072
tobre <tobre@dorpat>
2013-09-08 16:32:57 +0300

    Change-Id: I82ac9361f9adcd3ffce40affbfa22724f2ba6bd4

--- library/ZendService/Amazon/ProductAdvertising/BrowseNodeResult.php
+++ library/ZendService/Amazon/ProductAdvertising/BrowseNodeResult.php
@@ -54,7 +54,7 @@ class BrowseNodeResult
         /*$item = $this->xpath->query('//az:Item');
         $this->item = new Item($item);*/
         $browseNode = $xpath->query('//az:BrowseNodes/BrowseNode');
-        $this->BrowseNode = new BrowseNode($browseNode);
+        $this->BrowseNode = new BrowseNode($browseNode->item(0));
     }
 
     public function asXml()
